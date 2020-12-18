7ab21240b985a8939ca860514adeae6e35908db2
Erik Guzman <erik.guzman@guzman.codes>
2020-12-18 22:20:10 +0000

    Solve day 3 p2 and day 4 p1

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,29 +1,36 @@
 defmodule AdventOfCode.Day03 do
 
   @spec part1(any) :: nil
-  def part1(args) do
-    x = 3
-    y = 1
+  def part1(args, x \\ 3, y \\ 1) do
+    build_map_terrain(args)
+    |> start_traverse(x, y)
+  end
 
-    map_section = args
-    |> parse_rows
-    |> Enum.map(fn r -> parse_row(r) end)
+  def part2(args) do
+    map = build_map_terrain(args)
 
+    [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
+    |> Enum.map(fn [x, y] -> start_traverse(map, x, y) end)
+    |> Enum.reduce(fn v, acc -> v * acc end)
+  end
+
+  defp build_map_terrain(map_string) do
+    map_section = parse_map_section(map_string)
     height = Enum.count(map_section)
     width = Enum.count(Enum.at(map_section, 0))
     map_section_repeat = height * width
 
     # build the whole map
-    map = map_section
+    map_section
     |> Enum.map( fn row ->
       List.duplicate(row, map_section_repeat)
-      |> Enum.concat end
-    )
-
-    start_traverse(map, x, y)
+      |> Enum.concat end)
   end
 
-  def part2(args) do
+  defp parse_map_section(map_string) do
+    map_string
+    |> parse_rows
+    |> Enum.map(fn r -> parse_row(r) end)
   end
 
   defp parse_rows(s) do
--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,5 +1,33 @@
 defmodule AdventOfCode.Day04 do
+
   def part1(args) do
+    args
+    |> parse_passports
+    |> Enum.map(fn p -> validate(p) end)
+    |> Enum.count(fn x -> x == true end)
+  end
+
+  defp parse_passports(input) do
+    input
+    |> String.split(~r{\n\n})
+    |> Enum.map(fn p -> format_passport(p) end)
+  end
+
+  defp format_passport(input) do
+    input
+    |> String.split(~r{(\n|\s)})
+    |> Enum.reduce(%{}, fn v, acc -> format_data_field(v, acc) end)
+  end
+
+  defp format_data_field(input, acc) do
+    [key, value] = input |> String.split(":")
+    Map.put(acc, key, value)
+  end
+
+  defp validate(passport) do
+    required_field = [:byr, :iyr, :eyr , :hgt, :hcl, :ecl, :pid]
+
+    Enum.all?(required_field, fn field -> Map.has_key?(passport, to_string(field)) end)
   end
 
   def part2(args) do
